3c60d818a08583394f41a39a3cbd230ecf87004e
Tomas Jacik <tomas@jacik.cz>
2019-06-10 01:16:00 +0200

    Repository: removed unused methods

--- app/model/Repository.php
+++ app/model/Repository.php
@@ -4,7 +4,6 @@ namespace App\Model;
 
 use Nette;
 use Nette\Database\Context;
-use Nette\Database\Table\Selection;
 
 abstract class Repository
 {
@@ -18,43 +17,6 @@ abstract class Repository
     public function __construct(Context $database)
     {
         $this->database = $database;
-    }
-
-    public function uncamelize(string $camel, string $splitter = '_'): string
-    {
-        return strtolower(preg_replace(
-            '/(?!^)[[:upper:]][[:lower:]]/',
-            '$0',
-            preg_replace('/(?!^)[[:upper:]]+/', $splitter . '$0', $camel)
-        ));
-    }
-
-    protected function getTable(): Selection
-    {
-        // název tabulky odvodíme z názvu třídy
-        preg_match('#(\w+)Repository$#', get_class($this), $m);
-        return $this->database->table($this->uncamelize($m[1]));
-    }
-
-    public function getCount(): int
-    {
-        return $this->getTable()->count('*');
-    }
-
-    public function findAll(): Selection
-    {
-        return $this->getTable();
-    }
-
-    public function findBy(array $by): Selection
-    {
-        return $this->getTable()->where($by);
-    }
-
-    public function insert(array $values): string
-    {
-        $this->getTable()->insert($values);
-        return $this->database->getInsertId();
     }
 
     public function begin(): void
