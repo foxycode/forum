e9169a062331118b641f2b472e18d621f8c9c9d3
Tomas Jacik <tomas@jacik.cz>
2024-09-27 19:23:13 +0200

    SettingPresenter: refactoring

--- app/Presenters/SettingPresenter.php
+++ app/Presenters/SettingPresenter.php
@@ -4,11 +4,34 @@ namespace App\Presenters;
 
 use App\Core\UserManager;
 use Nette\Application\UI\Form;
+use Nette\Forms\Form as FormRule;
 use Nette\Security\SimpleIdentity;
 use Nette\Utils\ArrayHash;
 
 final class SettingPresenter extends BasePresenter
 {
+    private const array PerPageOptions = [
+        25 => 25,
+        50 => 50,
+        75 => 75,
+        100 => 100,
+        125 => 125,
+        150 => 150,
+        175 => 175,
+        200 => 200,
+    ];
+
+    private const array SortByOptions = [
+        'last_reply_time' => 'Času poslední odpovědi',
+        'create_time' => 'Času vytvoření',
+    ];
+
+    private const array StyleOptions = [
+        'forum.css' => 'Světle modrý',
+        'forum_d.css' => 'Tmavě modrý',
+        'forum_b.css' => 'Černý',
+    ];
+
     public function __construct(
         private readonly UserManager $userManager,
     ) {
@@ -19,54 +42,43 @@ final class SettingPresenter extends BasePresenter
     {
         $form = new Form();
 
-        $perpage = [
-            25 => 25,
-            50 => 50,
-            75 => 75,
-            100 => 100,
-            125 => 125,
-            150 => 150,
-            175 => 175,
-            200 => 200,
-        ];
-        $form->addSelect('perpage', 'Počet příspěvků', $perpage)
-            ->addRule(Form::Filled, 'Je nutné vybrat počet příspěvků');
+        $form->addSelect('perpage', 'Počet příspěvků', self::PerPageOptions)
+            ->addRule(FormRule::Filled, 'Je nutné vybrat počet příspěvků');
 
-        $sortby = [
-            'last_reply_time' => 'Času poslední odpovědi',
-            'create_time' => 'Času vytvoření',
-        ];
-        $form->addSelect('sortby', 'Řadit podle', $sortby)
-            ->addRule(Form::Filled, 'Je nutné vybrat řazení');
+        $form->addSelect('sortby', 'Řadit podle', self::SortByOptions)
+            ->addRule(FormRule::Filled, 'Je nutné vybrat řazení');
 
         $form->addPassword('oldPassword', 'Staré heslo')
-            ->setRequired(TRUE)
-            ->addRule(function ($item, $arg) {
-                return md5($item->value) == $arg;
-            }, 'Je nutné zadat platné heslo', $this->getUser()->getIdentity()->data['password']);
+            ->setRequired('Je nutné zadat staré heslo')
+            ->addRule(
+                fn ($item, $arg) => md5($item->value) == $arg,
+                'Je nutné zadat platné heslo',
+                $this->getUser()->getIdentity()->data['password']
+            );
 
-        $style = [
-            'forum.css' => 'Světle modrý',
-            'forum_d.css' => 'Tmavě modrý',
-            'forum_b.css' => 'Černý',
-        ];
-        $form->addSelect('style', 'Vzhled', $style)
-            ->addRule(Form::Filled, 'Je nutné vybrat vzhled');
+        $form->addSelect('style', 'Vzhled', self::StyleOptions)
+            ->addRule(FormRule::Filled, 'Je nutné vybrat vzhled');
 
-        $form->addText('mail', 'E-mail');
+        $form->addText('mail', 'E-mail')
+            ->addCondition(FormRule::Filled)
+                ->addRule(FormRule::Email, 'Musí jít o platný e-mail');
+
         $form->addText('icq', 'ICQ');
+
         $form->addText('jabber', 'Jabber');
+
         $form->addPassword('newPassword1', 'Nové heslo');
+
         $form->addPassword('newPassword2', 'Kontrola')
-            ->setRequired(FALSE)
-            ->addConditionOn($form['newPassword1'], Form::Filled)
-                ->addRule(Form::EQUAL, 'Hesla se neshodují', $form['newPassword1']);
+            ->setRequired(false)
+            ->addConditionOn($form['newPassword1'], FormRule::Filled)
+                ->addRule(FormRule::Equal, 'Hesla se neshodují', $form['newPassword1']);
 
         $form->addSubmit('send', 'Uložit');
 
         $form->setDefaults($this->getUser()->getIdentity()->data);
 
-        $form->onSuccess[] = [$this, 'userFormSuccess'];
+        $form->onSuccess[] = $this->userFormSuccess(...);
 
         return $form;
     }
